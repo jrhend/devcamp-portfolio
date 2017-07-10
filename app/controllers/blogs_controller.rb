f67ee8474f642b1c17463742a451770aa6d76fbd
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 14:49:01 +0000

    implemented collection based partials

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,7 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     @blogs = Blog.all
-    @page_title = "My Blog"
+    @page_title = "My Portfolio Blog"
   end
 
   # GET /blogs/1
@@ -32,7 +32,7 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+        format.html { redirect_to @blog, notice: 'Your post is now live.' }
       else
         format.html { render :new }
       end
@@ -56,18 +56,19 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: 'Post was removed.' }
       format.json { head :no_content }
     end
   end
-  
+
   def toggle_status
-    if  @blog.draft?
-      @blog.published! 
+    if @blog.draft?
+      @blog.published!
     elsif @blog.published?
-        @blog.draft!
+      @blog.draft!
     end
-    redirect_to blogs_url, notice: 'post status has been updated'
+        
+    redirect_to blogs_url, notice: 'Post status has been updated.'
   end
 
   private
